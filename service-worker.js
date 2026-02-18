b9a87815c5f6eaf14f02def10b031d156d19e23b
katoei0130-sukagimo <katoei0130@gmail.com>
2026-02-18 21:46:53 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,28 @@
-const CACHE_NAME = "mkw-memo-v1";
+const CACHE_NAME = "mkw-memo-v4";
+
 const urlsToCache = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
   );
 });
 
